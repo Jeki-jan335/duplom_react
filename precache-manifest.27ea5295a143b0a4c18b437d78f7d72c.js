a7981135b0e89e51ada7a4a75a74fc64cc17ae71
Jeki-jan335 <62606154+Jeki-jan335@users.noreply.github.com>
2020-05-21 22:58:37 +0300

    Updates

---
+++ precache-manifest.27ea5295a143b0a4c18b437d78f7d72c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f43c588cad06704136254a8f0d70d56",
+    "url": "/duplom_react/index.html"
+  },
+  {
+    "revision": "dbf499f0b2e8aadf7784",
+    "url": "/duplom_react/static/js/2.1c480718.chunk.js"
+  },
+  {
+    "revision": "d41120ca2603706247fb",
+    "url": "/duplom_react/static/js/main.b95d132a.chunk.js"
+  },
+  {
+    "revision": "925491329c9b1c8e7010",
+    "url": "/duplom_react/static/js/runtime~main.6af2f44b.js"
+  }
+]);
